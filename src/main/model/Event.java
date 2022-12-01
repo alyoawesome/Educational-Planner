ca5a6f12134b8503d57bd31cd5ae596914388cfd
alexyeung <alyo@students.cs.ubc.ca>
2022-11-30 22:02:17 -0800

    fixed checkstyle for Phase 4: Task 2

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 
 // CITATION: Reused this Event class from AlarmSystem
+
 /**
  * Represents an alarm system event.
  */
@@ -16,7 +17,8 @@ public class Event {
     /**
      * Creates an event with the given description
      * and the current date/time stamp.
-     * @param description  a description of the event
+     *
+     * @param description a description of the event
      */
     public Event(String description) {
         dateLogged = Calendar.getInstance().getTime();
@@ -25,7 +27,8 @@ public class Event {
 
     /**
      * Gets the date of this event (includes time).
-     * @return  the date of the event
+     *
+     * @return the date of the event
      */
     public Date getDate() {
         return dateLogged;
@@ -33,23 +36,25 @@ public class Event {
 
     /**
      * Gets the description of this event.
-     * @return  the description of the event
+     *
+     * @return the description of the event
      */
-    public String getDescription () {
+    public String getDescription() {
         return description;
     }
 
     @Override
     public boolean equals(Object other) {
-        if (other == null)
+        if (other == null) {
             return false;
-
-        if (other.getClass() != this.getClass())
+        }
+        if (other.getClass() != this.getClass()) {
             return false;
-
+        }
         Event otherEvent = (Event) other;
 
-        return (this.dateLogged.equals(otherEvent.dateLogged) &&
+        return (this.dateLogged.equals(otherEvent.dateLogged)
+                &&
                 this.description.equals(otherEvent.description));
     }
 
